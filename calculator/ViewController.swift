e0f7d350f9c950770d16826a53777261850d39f2
Shunsuke Sogaya <shunsuke@Shunsuke-no-MacBook-Air-2.local>
2017-12-03 19:46:32 +0900

    電卓アプリ

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -9,17 +9,100 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    //列挙型
+    enum Operator{
+        case undefined                      //未定義
+        case addition                       //加法
+        case subtraction                    //減法
+        case multiplication                 //乗法
+        case divition                       //除法
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    var firstValue = 0
+    var secondValue = 0
+    var currentOperator = Operator.undefined   //列挙型の値の取得
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBAction func numberButtonTapped(_ sender: UIButton) {
+        var value = 0
+        switch sender.currentTitle! {        //ボタンに表示されている文字列を取得
+            case "0":
+                value = 0
+            case "1":
+                value = 1
+            case "2":
+                value = 2
+            case "3":
+                value = 3
+            case "4":
+                value = 4
+            case "5":
+                value = 5
+            case "6":
+                value = 6
+            case "7":
+                value = 7
+            case "8":
+                value = 8
+            case "9":
+                value = 9
+            default:
+                value = 0
+        }
+        if currentOperator == Operator.undefined{
+            firstValue = firstValue * 10 + value
+            label.text = "\(firstValue)"
+        }else{
+            secondValue = secondValue * 10 + value
+            label.text = "\(secondValue)"
+        }
     }
-
-
+    
+    
+    @IBAction func operatorButtonTapped(_ sender: UIButton) {
+        switch sender.currentTitle! {
+        case "+":
+            currentOperator = .addition
+        case "-":
+            currentOperator = .subtraction
+        case "×":
+            currentOperator = .multiplication
+        case "÷":
+            currentOperator = .divition
+        default:
+            currentOperator = .undefined
+        }
+    }
+    
+    
+    @IBAction func equelButtonTapped(_ sender: UIButton) {
+        var value = 0
+        switch currentOperator {
+        case .addition:
+            value = firstValue + secondValue
+        case .subtraction:
+            value = firstValue - secondValue
+        case .multiplication:
+            value = firstValue * secondValue
+        case .divition:
+            value = firstValue / secondValue
+        default:
+            value = firstValue
+        }
+        label.text = "\(value)"
+        firstValue = 0
+        secondValue = 0
+        currentOperator = Operator.undefined
+    }
+    
+    
+    @IBAction func allClearButtonTapped(_ sender: UIButton) {
+        firstValue = 0
+        secondValue = 0
+        currentOperator = Operator.undefined
+        label.text = "0"
+    }
+    
 }
 
